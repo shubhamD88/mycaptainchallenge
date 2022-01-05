77c8131e6eab63301dc2c58bb449ed704b94f6c7
shubhamD88 <94123771+shubhamD88@users.noreply.github.com>
2022-01-05 12:54:47 +0530

    Add files via upload

---
+++ lettercount.py
@@ -0,0 +1,10 @@
+from collections import Counter
+def most_frequent(string="Mississippi"):
+    print("Please enter a string" '\n' ,string)
+most_frequent()    
+#letter counter in decreasing order   
+string = "Mississippi"
+string_length = Counter(string)
+reversed(string_length)
+print(string_length,)
+    
